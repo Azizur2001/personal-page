b45a16bfc02107a84ca3ba5b8bec79d490d9d4dd
Azizur Rahman <azizur.rahman59@qmail.cuny.edu>
2024-11-18 23:07:20 -0500

    Fixed the layout

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import HeroSection from './components/HeroSection';
-// import Services from './components/Services';
-// import Projects from './components/Projects';
-// import About from './components/About';
-// import Contact from './components/Contact';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div style={{ overflowX: 'hidden' }}>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<HeroSection />} />
-//           <Route path="/services" element={<Services />} />
-//           <Route path="/projects" element={<Projects />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/contact" element={<Contact />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import Services from './components/Skills';
-import Projects from './components/Projects';
-import About from './components/About';
-import Contact from './components/Contact';
-import Skills from './components/Skills';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Services from "./components/Skills";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
 
 const App = () => {
   return (
     <Router>
-      <div style={{ overflowX: 'hidden', scrollBehavior: 'smooth' }}>
+      <div style={{ overflowX: "hidden", scrollBehavior: "smooth" }}>
         <Navbar />
         {/* Single Page Rendering for all sections */}
         <Routes>
@@ -55,7 +24,6 @@ const App = () => {
                 <Skills />
                 <Projects />
                 <Contact />
-
               </>
             }
           />
